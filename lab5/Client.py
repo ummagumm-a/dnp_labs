0bdc60a7c39adbbb62bb54a7d80b0ad3eaf6cf03
Sinii Viacheslav <siniy.vyacheslav@gmail.com>
2022-10-09 23:33:50 +0300

    Client program sketch

---
+++ lab5/Client.py
@@ -0,0 +1,171 @@
+import sys
+import grpc
+import chord_pb2 as pb2
+import chord_pb2_grpc as pb2_grpc
+from typing import Callable
+import re
+
+class ClientConnection:
+    def __init__(self):
+        self.server_type = None
+        self.stub = None
+
+    def _stub_type_check_and_execute(self, node_callback: Callable, registry_callback: Callable):
+        """
+        Checks the validity of the stub's type and performs appropriate operation depending on the type.
+
+        :params node_callback:
+        """
+        if self.stub is None:
+            raise Exception("Connection is not created yet.")
+        elif isinstance(self.stub, pb2_grpc.NodeStub):
+            return node_callback()
+        elif isinstance(self.stub, pb2_grpc.RegistryStub):
+            return registry_callback()
+        else:
+            raise Exception(f"Weird stub type: {type(self.stub)}.")
+        
+    def connect(self, ipaddr: str, port: int):
+        """
+        Connect to a node or a registry with given address. 
+        If connection is successful - stub is initialized and saved. Otherwise, raises an exception.
+
+        TODO: Should distinguish between node and registry. Now connects only to nodes.
+        """
+
+        try:
+            channel = grpc.insecure_channel(f'{ipaddr}:{port}')
+            self.stub = pb2_grpc.NodeStub(channel)
+        except:
+            raise Exception(f"Couldn't create a connection to {ipaddr}:{port}.")
+
+    def get_info(self):
+        """
+        If is connected to a node - return finger table.
+        If is connected to a registy - return chord info.
+
+        Both in string representation. This function is only used for visualization purposes.
+        """
+        def node_callback():
+            return str(self.stub.get_finger_table())
+
+        def registry_callback():
+            return str(self.stub.get_chord_info())
+        
+        return self._stub_type_check_and_execute(node_callback, registry_callback)
+
+    def save(self, key: str, text: str):
+        """
+        Saves a key-value pair into Chord.
+
+        Returns message about success of performed operation.
+
+        :params key: key
+        :params text: value
+        """
+        def node_callback():
+            save_request = pb2.SaveRequest(key=key, text=text)
+            _, message = self.stub.save_key(save_request)
+
+            return message
+
+        def registry_callback():
+            raise Exception("Can't save this data. The client is connected to a registry.")
+
+        return self._stub_type_check_and_execute(node_callback, registry_callback)
+    
+    def remove(self, key: str):
+        """
+        Remove a key-value pair from Chord.
+
+        Returns message about success of performed operation.
+
+        :params key: key
+        """
+        def node_callback():
+            remove_request = pb2.RemoveRequest(key=key)
+            _, message = self.stub.remove_key(remove_request)
+
+            return message
+
+        def registry_callback():
+            raise Exception("Can't remove this key. The client is connected to a registry.")
+
+        return self._stub_type_check_and_execute(node_callback, registry_callback)
+
+    def find(self, key):
+        """
+        Finds a value by key in Chord.
+        If operation is success - returns the text. Otherwise, returns an error message.
+
+        :params key: key
+        """
+        def node_callback():
+            find_request = pb2.FindRequest(key=key)
+            result, message, _ = self.stub.find_key(find_request)
+
+            if result:
+                return message
+            else:
+                return f"Key wasn't found: {message}"
+
+        def registry_callback():
+            raise Exception("Can't find this key. The client is connected to a registry.")
+
+        return self._stub_type_check_and_execute(node_callback, registry_callback)
+
+def make_args(match: re.Match):
+    """
+    Creates a list of arguments parsed from matched string.
+    """
+    args = []
+    i = 2
+    while True:
+        try:
+            arg = match[i]
+        except:
+            return args
+        i += 1
+        args.append(args)
+
+def parse_input(inp: str):
+    """
+    Accepts an input string and returns query with arguments.
+    """
+    # List patterns of acceptable messages
+    connect_regex = "(connect) (\d+.\d+.\d+.\d+) (\d+)"
+    get_info_regex = "(get_info)"
+    save_regex = '(save) "(\w+)" "(\w+)"'
+    remove_regex = "(remove) (\w+)"
+    find_regex = "(find) (\w+)"
+
+    # Compile them into regex patterns
+    patterns = [connect_regex, get_info_regex, save_regex, remove_regex, find_regex]
+    patterns = list(map(re.compile, patterns))
+
+    # Check each pattern
+    for pattern in patterns:
+        match = pattern.match(inp)
+        # If matched successfully
+        if match is not None:
+            return match[1], make_args(match)
+    
+    return None, None
+
+def cli_loop():
+    client_connection = ClientConnection()
+
+    while True:
+        inp = input("> ")
+        if inp == 'exit':
+            break
+        query, args = parse_input(inp)
+        if query is None:
+            print("Incorrect Query")
+            continue
+        res = eval(f'client_connection.{query}')(*args)
+        print(res)
+
+if __name__ == '__main__':
+    cli_loop()
+
