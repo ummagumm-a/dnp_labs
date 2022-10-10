94560fa83d03d838f4c603b43914219a9a871213
Sinii Viacheslav <siniy.vyacheslav@gmail.com>
2022-10-11 00:24:33 +0300

    comments + function for comparing nodes in a ring

--- lab5/tests/test_utils.py
+++ lab5/tests/test_utils.py
@@ -1,7 +1,5 @@
-import sys
-print(sys.path)
 import unittest
-from utils import get_succ, get_pred
+from utils import get_succ, get_pred, ring_between
 
 
 class UtilsTest(unittest.TestCase):
@@ -18,6 +16,15 @@ class UtilsTest(unittest.TestCase):
         self.assertEqual(get_pred(5, other_ids), 2)
         self.assertEqual(get_pred(21, other_ids), 20)
         self.assertEqual(get_pred(1, other_ids), 20)
+
+    def test_ring_between(self):
+        self.assertTrue(ring_between(16, 17, 18))
+        self.assertFalse(ring_between(17, 16, 18))
+        self.assertFalse(ring_between(18, 17, 16))
+        self.assertTrue(ring_between(17, 18, 16))
+        self.assertFalse(ring_between(16, 18, 17))
+        self.assertTrue(ring_between(18, 16, 17))
+        self.assertTrue(ring_between(16, 17, 16))
 
 if __name__ == '__main__':
     unittest.main()
--- lab5/utils.py
+++ lab5/utils.py
@@ -2,6 +2,10 @@ def mod(a: int, b: int) -> int:
     return (a % b + b) % b
     
 def get_succ(id, other_ids):
+    """
+    Find successor of 'id' in ring 'other_ids'.
+    """
+
     gt_id = [other_id for other_id in other_ids if other_id > id]
 
     if len(gt_id) != 0:
@@ -10,9 +14,25 @@ def get_succ(id, other_ids):
         return min(other_ids)
 
 def get_pred(id, other_ids):
+    """
+    Find predecessor of 'id' in ring 'other_ids'.
+    """
+
     lt_id = [other_id for other_id in other_ids if other_id < id]
 
     if len(lt_id) != 0:
         return max(lt_id)
     else:
         return max(other_ids)
+
+def ring_between(left, num, right):
+    """
+    Calculate whether 'num' is in between 'left' and 'right' on a number ring.
+    """
+
+    if left < right:
+        return left < num < right
+    elif left > right:
+        return left < num or num < right
+    else:
+        return True
